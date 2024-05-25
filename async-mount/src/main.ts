384ef75398c2a19336169c35400143fc7a52be8b
sebkolind <sks1993@gmail.com>
2024-05-25 12:23:13 +0200

    docs(async-mount): add attribute assignment comment

--- async-mount/src/main.ts
+++ async-mount/src/main.ts
@@ -7,6 +7,8 @@ fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
     const target = document.querySelector<HTMLDivElement>(".target");
 
     if (target) {
+      // Set the JSON string to the `data-todos` attribute,
+      // so that it can be accessed with `el.dataset.todos` in the component.
       target.dataset.todos = JSON.stringify(json);
 
       mount(target, AsyncMount);
